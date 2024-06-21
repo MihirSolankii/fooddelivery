c8875761bf848028ed533e45281491c1ef77a7a0
mihirso <127624978+mihirso@users.noreply.github.com>
2024-06-21 12:24:33 +0530

    Update Header.js

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import './Header.css'
+import { assets } from '../../assets/assets'
 function Header() {
   return (
     <div className='header'>
+    <img src={assets.header_img}/>
       <div className='header-contents'>
         <h2>Order Your Favourite Food Here</h2>
         <p>choose from a  diverse menu featuring a delectable array of dishes crafted wit the finest ingredients and culinary expertise. our mission is to satisfy ypur cravings and elevate your dining experience,one delicious meal at a time</p>
